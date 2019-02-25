f19374725fdba7ceef2081f412819363fbae20c9
Mesrob <mesrobk.dev@gmail.com>
2019-02-24 20:22:39 -0800

    adding material UI

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 class App extends React.Component {
   render() {
     return (
-      <div>hello</div>
+      <div><Button variant="contained" color="primary">Barev</Button></div>
     )
   }
 }
