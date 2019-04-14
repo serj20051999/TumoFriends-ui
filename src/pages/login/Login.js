f79cdcce70e154a1d4c855c958d0074da9709cad
Mesrob <mesrobk.dev@gmail.com>
2019-04-14 15:00:43 -0700

    adding sample login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Login extends Component {
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import {Container} from 'react-bootstrap';
+
+export default class Login extends Component {
   render() {
     return (
-      <div className="text-center">Create a login form!</div>
+      <Container>
+        <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            Login with your TUMO username and password.
+        </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        </Form>
+      </Container>
     )
   }
 }
-
-export default Login;
