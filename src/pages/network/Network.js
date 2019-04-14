617f49b1564f471bb81b5a89bc882ac9214df1cd
Mesrob <mesrobk.dev@gmail.com>
2019-04-14 16:26:23 -0700

    remove code from default network page

--- src/pages/network/Network.js
+++ src/pages/network/Network.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import ListGroup from 'react-bootstrap/ListGroup';
 import {Container, Row, Col} from 'react-bootstrap';
 
 import './network.css';
@@ -11,15 +10,7 @@ class NetworkPage extends Component {
       <Container fluid={true} className="p-0">
         <Row noGutters={true}>
           <Col xs={2}>
-            <div className="sidenav">
-              <ListGroup>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-              </ListGroup>
-            </div>
+            <div>Placeholder for student list</div>
           </Col>  
           <Col>
             <div>Collaboration Page Placeholder</div>
