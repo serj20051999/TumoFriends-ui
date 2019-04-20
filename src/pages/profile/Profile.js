cb5478e518082806d6b616f488e475dc63978ce3
Mesrob <mesrobk.dev@gmail.com>
2019-04-20 15:27:30 -0700

    Adding profile view

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,11 +1,100 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Redirect} from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.learningTargets = ["Animation", "Game Development", "Filmmaking", "Web Development"]
+    this.locations = ["Yerevan",  "Gyumri", "Stepnakert", "Dilijan"]
+    this.state = {
+      email: props.user ? props.user.email : '',
+      firstName: props.user ? props.user.firstName: '',
+      lastName: props.user ? props.user.lastName: '',
+      password: props.user ? props.user.password: '',
+      learningTargets: props.user ? props.user.learningTargets : [],
+      location: props.user ? props.user.location : this.locations[0]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createUser(this.state);
+  }
+  handleChange(type, value) {
+    if (type === "learningTargets") {
+      const valueAsArray = Array.from(value); // Convert HTMLCollections to Array
+      const learningTargets = valueAsArray.filter(v => v.selected).map(v => v.value); // Filter and map to option value
+      this.setState({
+        learningTargets,
+      })
+    } else {
+      this.setState({
+        [type]: value
+      });
+    } 
+  }
   render() {
+    if (this.props.user == null) {
+      return (
+        <Redirect to={{
+          pathname: '/',
+        }} />
+      )
+    }
     return (
-      <div className="text-center">Create a Profile form!</div>
+      <Container>
+        <Form className="mt-5" onSubmit={e => this.handleSubmit(e)}>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control value={this.state.email} onChange={(e) => { this.handleChange("email", e.target.value) }} type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control value={this.state.firstName} onChange={(e) => { this.handleChange("firstName", e.target.value) }} type="text" placeholder="Enter first name" />
+          </Form.Group>
+          <Form.Group controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control value={this.state.lastName} onChange={(e) => { this.handleChange("lastName", e.target.value) }} type="text" placeholder="Enter last name" />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Passowrd</Form.Label>
+            <Form.Control value={this.state.password} onChange={(e) => { this.handleChange("password", e.target.value) }} type="password" placeholder="Enter password" />
+          </Form.Group>
+          <Form.Group controlId="formLearningTarget">
+            <Form.Label>Learning Targets</Form.Label>
+            <Form.Control value={this.state.learningTargets} onChange={(e) => { this.handleChange("learningTargets", e.target.options) }} as="select" multiple>
+              {
+                this.learningTargets.map(target => 
+                  <option value={target} key={target}>{target}</option>
+                )
+              }
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="formLocation">
+            <Form.Label>Location</Form.Label>
+            <Form.Control value={this.state.location} onChange={(e) => { this.handleChange("location", e.target.value) }} as="select">
+              {
+                this.locations.map(location => 
+                  <option key={location}>{location}</option>
+                )
+              }
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Update
+          </Button>
+        </Form>
+      </Container>      
     )
   }
+}
+
+Profile.propTypes = {
+  createUser: PropTypes.func,
+  user: PropTypes.object,
 }
 
 export default Profile;
--- src/pages/profile/ProfileContainer.js
+++ src/pages/profile/ProfileContainer.js
@@ -1,9 +1,20 @@
 import { connect } from 'react-redux'
 
 import Profile from './Profile';
+import { createUser } from '../../redux/actions';
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => {
+  return {
+    createUser: ({email, password, firstName, lastName, learningTargets, location}) => {
+      dispatch(createUser(email, password, firstName, lastName, learningTargets, location));
+    }
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
