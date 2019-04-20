9e74f2173353cf13768bdb626d78e894eec7a222
Mesrob <mesrobk.dev@gmail.com>
2019-04-20 14:04:42 -0700

    creating signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,64 @@
 import React, {Component} from 'react';
 
+import { Container, Form, Button } from 'react-bootstrap';
+
 class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.learningTargets = ["Animation", "Game Development", "Filmmaking", "Web Development"]
+    this.locations = ["Yerevan",  "Gyumri", "Stepnakert", "Dilijan"]
+  }
+  handleSubmit() {
+
+  }
+  handleChange() {
+
+  }
   render() {
     return (
-      <div className="text-center">Create a signup form!</div>
+      <Container>
+        <Form className="mt-5">
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter first name" />
+          </Form.Group>
+          <Form.Group controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter last name" />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Passowrd</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" />
+          </Form.Group>
+          <Form.Group controlId="formLearningTarget">
+            <Form.Label>Learning Targets</Form.Label>
+            <Form.Control as="select" multiple>
+              {
+                this.learningTargets.map(target => 
+                  <option>{target}</option>
+                )
+              }
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="formLocation">
+            <Form.Label>Location</Form.Label>
+            <Form.Control as="select">
+              {
+                this.locations.map(location => 
+                  <option>{location}</option>
+                )
+              }
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Signup
+          </Button>
+        </Form>
+      </Container>      
     )
   }
 }
--- src/pages/signup/SignupContainer.js
+++ src/pages/signup/SignupContainer.js
@@ -1,9 +1,20 @@
 import { connect } from 'react-redux'
 
 import Signup from './Signup';
+import { loginUser } from '../../redux/actions';
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => {
+  return {
+    createUser: (email, password, firstName, lastName) => {
+      dispatch(loginUser(email, password));
+    }
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Signup);
