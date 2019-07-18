8ca5f18244972f10b07b696de9e54cfb1c577db1
serj20051999 <serj.ohanyan.y@tumo.org>
2019-07-18 16:53:38 +0400

    changed login file

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,27 +2,68 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 // TODO: use --> import {Redirect} from 'react-router-dom';
-import {Container} from 'react-bootstrap';
-
+import {Form, Button, Container} from 'react-bootstrap';
+import './Login.css';
 /**
  * Component for Login Page
  */
 export default class Login extends Component {
-  // constructor(props) {
-    // TODO: set state and form handlers
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+ this.handleSubmit = this.handleSubmit.bind(this);
+  }
+ 
+  handleChange(event) {
+    this.setState({
+      [event.target.type]: event.target.value
+    }); 
+  }
+ 
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.email);
+    event.preventDefault();
+  }
+ 
+
   render() {
-    // TODO: use to redirect if user not logged in
-    // if (this.props.user) {
-    //   return (
-    //     <Redirect to={{
-    //       pathname: '/profile',
-    //     }} />
-    //   )
-    // }
+  //   // TODO: use to redirect if user not logged in
+  //   // if (this.props.user) {
+  //   //   return (
+  //   //     <Redirect to={{
+  //   //       pathname: '/profile',
+  //   //     }} />
+  //   //   )
+  //   // }
+   
+    
     return (
       <Container className="mt-3">
-        <div>TODO: create</div>
+      <h1 className="heading">Login</h1>
+      <Form onSubmit={this.handleSubmit } className="form" > 
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" onChange={this.handleChange} />
+    <Form.Text className="text-muted">
+    Login with your TUMO username and password.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" onChange={this.handleChange} />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+  <hr />
+  <p className="h5">Don't have an account? <a className="text" href="/signup">Sign-up to connect!</a></p>
+</Form>
       </Container>
     )
   }
